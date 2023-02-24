6c4a5b7dd7b31280df23bbb20ed67251ba80fe2a
Alessandro Capelli <alessandrocapelli@MacBook-Pro.local>
2023-02-24 00:01:31 +0000

    CRUD with NET Core 6 & Angular 14 & EF Core & SQL Server for backend

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins", policy => { policy.WithOrigins("http://localhost:55203").AllowAnyMethod().AllowAnyHeader(); }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("SuperHeroOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
